e41ca14c81e5580b15469ca74070ab6478ef3b39
martapastuszka <marta.pastuszka.mp@gmail.com>
2020-06-28 21:17:55 +0200

    class Wallet finished

--- JednorekiBandyta/Wallet.js
+++ JednorekiBandyta/Wallet.js
@@ -9,6 +9,21 @@ class Wallet {
             if(_money >=value) return true;
             return false;
         }
+
+        this.changeWallet = (value, type = '+') =>{
+            if (typeof value === 'number' && !isNaN(value)){
+                if(type === '+'){
+                    return _money +=value;
+                }else if (type === '-'){
+                    return _money -=value;
+                }else {
+                    throw new Error('nieprawidłowy typ działania')
+                }
+            }else{
+                console.log(typeof value);
+                throw new Error ('nieprawidłowa liczba')
+            }
+        }
     }
 }
 
