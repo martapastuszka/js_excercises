b037312c7d7b1fcb1ab7d42c34b56b7d588dcf21
martapastuszka <marta.pastuszka.mp@gmail.com>
2020-11-06 21:42:31 +0100

    By clicking on a card, color displayed. Card colors are visible for 2 sec, then hidden. Max two cards visible at time.

--- MemoryGame/main.js
+++ MemoryGame/main.js
@@ -2,22 +2,55 @@ const cardColors = ['red','red', 'blue','blue', 'yellow', 'yellow', 'green', 'gr
 
 cardColors.sort(() => Math.random() - 0.5);
 const divItems = [...document.getElementsByTagName('div')];
-console.log(divItems);
+// console.log(divItems);
 // divItems[1].className='red';
-function divColored(){
+function showAllCards(){
     for(i=0; i<divItems.length; i++){
         divItems[i].className=cardColors[i]; 
     }
 }
 
-function divHidden(){
+function hideAllCards(){
     for(i=0; i<divItems.length;i++){
         divItems[i].className='hidden';
     }
 }
 
-divColored();
-setTimeout(divHidden, 2000);
+// let start;
+
+// function startTimer(){
+//     // console.log (start);
+//     start = Math.floor(Date.now()/1000);
+//     let timeCount = setTimeout(startTimer, 1000);
+// }
+
+// function stopTimer(){
+//     let stop = Math.floor(Date.now()/1000);
+//     let timeOfGame = stop - start;
+// }
+
+let visibleCards = 0;
+
+const showCard =(e) =>{
+    visibleCards++;
+    if(visibleCards<=2) e.target.className=cardColors[e.target.dataset.key];
+    if(visibleCards==2) setTimeout(twoSecondTimer, 1000);
+}
+
+const twoSecondTimer = function(){
+    hideAllCards();
+    visibleCards=0;
+}
+
+//muszę mieć więcej czynności wykonywanych na onload - poprawić
+showAllCards();
+setTimeout(hideAllCards, 2000);
+
+
+// document.addEventListener(onload, startTimer());
+
+const myCard = document.querySelectorAll('div').forEach(item=> item.addEventListener('click', showCard));
+
 
 
 
