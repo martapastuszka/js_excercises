3c9c2117a4dcdbde8c349ae7f30368f6d8a674c2
martapastuszka <marta.pastuszka.mp@gmail.com>
2020-11-11 12:36:54 +0100

    restart game after alert, code cleanup

--- MemoryGame/main.js
+++ MemoryGame/main.js
@@ -2,20 +2,22 @@ const cardColors = ['red','red', 'blue','blue', 'yellow', 'yellow', 'green', 'gr
 
 cardColors.sort(() => Math.random() - 0.5);
 const divItems = [...document.getElementsByTagName('div')];
-// console.log(divItems);
-// divItems[1].className='red';
+
+//przypisuję divom kolory
 function showAllCards(){
     for(i=0; i<divItems.length; i++){
         divItems[i].className=cardColors[i]; 
     }
 }
 
+// ukrywam karty, jeśli para ma różne kolory
 function hideAllCards(){
     for(i=0; i<divItems.length;i++){
         if(divItems[i].className !='off') divItems[i].className='hidden';
     }
 }
 
+// włączam timer liczący czas gry
 let start;
 let timeOfGame;
 function startTimer(){
@@ -27,26 +29,31 @@ function stopTimer(){
     timeOfGame = stop - start;
 }
 
+// odkrywam karty na kliknięcie i porównuję ich kolory
 let visibleCards = 0;
 let cardToBeComparedWith;
 
 const showCard =(e) =>{
     if((typeof cardToBeComparedWith==='undefined') || (cardToBeComparedWith.dataset.key!=e.target.dataset.key && e.target.className !='off')){
         visibleCards++;
-        // console.log(e.target.dataset.key);
         if(visibleCards<=2) e.target.className=cardColors[e.target.dataset.key];
         if(visibleCards==1) cardToBeComparedWith=e.target;
         if(visibleCards==2){
             setTimeout(halfSecondTimer, 500);
-            if(cardToBeComparedWith.className==e.target.className){
-                // console.log('takie same');
-                cardToBeComparedWith.className = 'off';
-                e.target.className='off';
-            } 
+            removingCards(e) 
         }
     }
 }
 
+//ukrywam parę kard o tym samym kolorze. 
+const removingCards = function(e){ 
+     if(cardToBeComparedWith.className==e.target.className){
+        cardToBeComparedWith.className = 'off';
+        e.target.className='off';
+    } 
+}
+
+// sprawdzam, czy wczystkie karty odkryte, jeżeli tak, kończę grę i wyświetlam czas gry
 const halfSecondTimer = function(){
     hideAllCards();
     visibleCards=0;
@@ -57,19 +64,23 @@ const halfSecondTimer = function(){
         }
         if(divItems.length == flag){
             stopTimer();
-            alert (`Gratulacje. Twój czas gry to ${timeOfGame} sekund!`)
+            alert (`Gratulacje. Twój czas gry to ${timeOfGame} sekund!`);
+            executeAfterLoading();
         }
     }
 }
 
-//muszę mieć więcej czynności wykonywanych na onload - poprawić
-showAllCards();
-setTimeout(hideAllCards, 2000);
+// elementy do wykonania po załadowaniu strony
 
+function executeAfterLoading(){
+    showAllCards();
+    setTimeout(hideAllCards, 2000);
+    startTimer();
+    const myCard = document.querySelectorAll('div').forEach(item=> item.addEventListener('click', showCard));
+}
 
-document.addEventListener(onload, startTimer());
+document.addEventListener(onload, executeAfterLoading());
 
-const myCard = document.querySelectorAll('div').forEach(item=> item.addEventListener('click', showCard));
 
 
 
